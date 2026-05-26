a3727712c7d04aabee07c0821bed9ec8b0fca9ec
Yaroslav Petrushko <yaroslavpetrushko.off@gmail.com>
2026-05-26 09:51:09 +0300

    Added database connector for SQLite, with initial database creation on startup (if not exist).

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,28 @@
+using FightingTournament.Services;
+using System;
 using System.Windows;
 
 namespace FightingTournament;
 
-public partial class App : Application { }
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        try
+        {
+            DatabaseConnector.Instance.InitializeDatabase();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(
+                $"Failed to initialize the database:\n\n{ex.Message}",
+                "Database Initialization Error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+            
+            Shutdown();
+        }
+    }
+}
